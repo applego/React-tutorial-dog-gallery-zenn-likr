69359be9f480265161787cb8223a551487587ba9
applego <applegorillappa@gmail.com>
2021-01-28 23:57:48 +0900

    TypeError: Cannot read property 'value' of undefined  Search.jsxでエラー

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,58 @@
+import React, { useState, useEffect, useCallback } from 'react';
 import './assets/styles/style.css';
-import { Header,ImageList,Search } from './components/index';
+import { Header, ImageList, Search } from './components/index';
 
-function App() {
+import objBreedList from './dataBreedList';
+
+const App = () => {
+  // const te = objBreedList.map(v => v.breed);
+  // alert(te[0]);
+
+  const [breedList, setBreedList] = useState(objBreedList);
+  const [currentBreed, setCurrentBreed] = useState("hound");
+  const [imageCnt, setImageCnt] = useState(10);
+  const [imageList, setImageList] = useState([]);
+
+
+  const handleChange = useCallback((event) => {
+    setCurrentBreed(event.target.value);
+  }, [setCurrentBreed]);
+
+  useEffect(() => {
+    (async () => {
+
+      const initImageList = [];
+
+      const initReqUrl = `https://dog.ceo/api/breed/${currentBreed}/images/random/${imageCnt}`;
+      console.log(initReqUrl);
+      // const ttt = (await fetch(initReqUrl)).json();
+      // console.log(ttt);
+      await fetch(initReqUrl)
+        .then((response) => {
+          response.json()
+            .then((json) => {
+              json.message.forEach(image => {
+                initImageList.push(image);
+              });
+            });
+        });
+
+      setImageList(initImageList);
+    })();
+  }, []);
+
   return (
     <div>
       <Header header={"from App.jsx"}></Header>
       <div className="main">
         <div>
           <h2>Search↓</h2>
-          <Search></Search>
+          <Search
+            breedList={breedList}
+            currentBreed={currentBreed}
+            handleChange={handleChange}
+          >
+          </Search>
         </div>
         <div>
           <h2>ImageList↓</h2>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-const Serach = () => {
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const Search = (props) => {
+  const classes = useStyles();
+  // console.log(props.breedList[2]);
+  props.breedList.forEach((breed) => {
+    console.log(breed.index);
+    console.log(breed.breed);
+  });
+  console.log(props.currentBreed);
+  // console.log(props.objBreedList[3]);
+
   return (
-    <div>Serach.jsx</div>
+    <div>
+      <h3>Serach.jsx</h3>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Breed</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={props.currentBreed}
+          onChange={props.handleChange}
+        >
+          {props.breedList.map((objBreed) => {
+            console.log(objBreed);
+            return <MenuItem key={objBreed.index.toString()} value={objBreed.breed}>{objBreed.breed}</MenuItem>
+          })};
+          <MenuItem value={10}>Ten</MenuItem>
+        </Select>
+      </FormControl>
+
+
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={10}
+          onChange={props.handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+
+    </div>
   );
 }
 
-export default Serach;
+export default Search;
